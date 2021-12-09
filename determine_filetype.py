bc4fd7600da6c1ce0e731742b1cebcb19e5b1c20
Shilpa N <s-n@ti.com>
2021-12-09 13:51:03 +0530

    Commiting file for determining filetype

---
+++ determine_filetype.py
@@ -0,0 +1,37 @@
+import requests
+import mimetypes
+import magic
+import pathlib
+
+user_url="/home/a0492783/Py_Acronym_processing/myfile.pdf"
+isLocal=True;
+
+url_to_file = ""
+local_file_path = ""
+filetype= ""
+
+if(not isLocal):
+    url_to_file = user_url;
+
+    r = requests.get(url_to_file)
+    content_type = r.headers.get('content-type')
+    ext=""
+
+    if 'application/pdf' in content_type:
+        ext = '.pdf'
+    elif 'text/html' in content_type:
+        ext = '.html'
+    else:
+        ext = ''
+        print('Unknown type: {}'.format(content_type))
+
+    print("Remote file, type = ",ext)
+    filetype=ext;
+else:
+    local_file_path = user_url;
+
+    #filetype = mimetypes.guess_extension(local_file_path)
+    #filetype = magic.from_file(local_file_path, mime = True)
+    filetype = pathlib.Path(local_file_path).suffix
+    print("Local file, type = ",filetype)
+
