aba3f871bf6caafd7928c75654ed677c2533f8c5
meenasomisetty <50726703+meenasomisetty@users.noreply.github.com>
2021-12-09 14:21:22 +0530

    Update url2text.py
    Used pdfminer for pdf to text instead of pypdf2

--- url2text.py
+++ url2text.py
@@ -1,32 +1,27 @@
-import PyPDF2 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import io
 import requests
 import urllib.request
-from pathlib import Path
+from pdfminer.high_level import extract_text
 
-# ------- Extracting text from a PDF file existing locally ------------    
+# ------- Extracting text from a PDF URL ------------    
 
-# creating a pdf file object 
-pdfFileObj = open('PDF_dataset/pdf2.pdf', 'rb') 
-    
-# creating a pdf reader object 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-   
-text = ""
-for i in range(pdfReader.numPages):
-    # creating a page object  
-    pageObj = pdfReader.getPage(i) 
-    # extracting text from page
-    text = text + pageObj.extractText()
+url = "https://arxiv.org/pdf/2112.03918.pdf"
+r = requests.get(url)
+ 
+# the HTTP response in a response object called r
+with open("filename.pdf",'wb') as fi:
+  
+    # Saving received content as a pdf file in
+    # binary format
+  
+    # write the contents of the response (r.content)
+    # to a new file in binary mode.
+    fi.write(r.content)
 
-"""with open("output1.txt",'w') as o1:
-  o1.write(text)"""
-print(text) 
-    
-# closing the pdf file object 
-pdfFileObj.close()
+text = extract_text('filename.pdf')
+print(text)
 
 # ------- Extracting text from a HTML URL ------------    
 url = "http://kite.com"
@@ -48,41 +43,5 @@ text2 = ' '.join(line for line in lines if line)
 
 print(text2)
 
-# ------- Extracting text from a PDF URL ------------    
-
-#url2 = 'http://ceur-ws.org/Vol-2150/BARR2_paper2.pdf'
-#url2 = "http://www.africau.edu/images/default/sample.pdf"
-url2 = "https://arxiv.org/pdf/2112.03918.pdf"
-r = requests.get(url2)
- 
-# the HTTP response in a response object called r
-with open("filename2.pdf",'wb') as fi:
-  
-    # Saving received content as a png file in
-    # binary format
-  
-    # write the contents of the response (r.content)
-    # to a new file in binary mode.
-    fi.write(r.content)
-
-# creating a pdf file object 
-pdfFileObj2 = open('filename2.pdf', 'rb') 
-    
-# creating a pdf reader object 
-pdfReader2 = PyPDF2.PdfFileReader(pdfFileObj2) 
-   
-text3 = ""
-for i in range(pdfReader2.numPages):
-    # creating a page object  
-    pageObj2 = pdfReader2.getPage(i) 
-    # extracting text from page
-    pagetext = pageObj2.extractText()
-    text3 = text3 + pagetext
-
-
-"""with open("output2.txt",'w') as o2:
-  o2.write(text3)"""
-print(text3)    
-print("\n ---------------------------------------------------------------- \n")
 
 
